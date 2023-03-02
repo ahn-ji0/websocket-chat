98b0ac8ecb56888c34b2dccd4f8520756b15e329
Jiyoung <ahn.jiyoung15@gmail.com>
2023-03-02 13:08:56 +0900

    Update: endpoint 수정

--- src/main/java/com/chat/websocketchat/v1/WebSocketConfig.java
+++ src/main/java/com/chat/websocketchat/v1/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+        registry.addHandler(chatHandler, "ws/chat/v1").setAllowedOrigins("*");
     }
 }
